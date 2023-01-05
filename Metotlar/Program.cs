193b6d3f26ac212f42cb5b29f312fe6dd33ce7db
filiz <filiz@FILIZ>
2023-01-05 14:44:06 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,51 @@
+﻿using Metotlar;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        string[] pandora = new string[] { };
+
+        Urun urun1 = new Urun();
+        urun1.Adi = "Bileklik";
+        urun1.Fiyati = 2600;
+        urun1.Aciklama = "Saf Gümüş";
+
+        Urun urun2 = new Urun();
+        urun2.Adi = "Kolye";
+        urun2.Fiyati = 5600;
+        urun2.Aciklama = "Saf Altın";
+        Urun[] urunler = new Urun[] { urun1, urun2 };
+
+        foreach (Urun urun in urunler)
+        {
+            Console.WriteLine(urun.Adi);
+            Console.WriteLine(urun.Fiyati);
+            Console.WriteLine(urun.Aciklama);
+            Console.WriteLine("------------------");
+
+
+
+
+
+            SepetManager sepetmanager = new SepetManager();
+            sepetmanager.Ekle(urun1);
+            sepetmanager.Ekle(urun2 );
+
+            sepetmanager.Ekle2("Bileklik", "Saf Gümüş", 2600 ,10);
+            sepetmanager.Ekle2("Kolye", "Saf Altın", 5600,20);
+
+
+
+
+        }
+
+
+
+
+
+
+        Console.WriteLine();
+    }
+}
---
+++ kamp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace kamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety -tip güvenliği
+            string kategoriEtiketi = "Katagori";
+            Console.WriteLine(kategoriEtiketi);
+
+
+            //dizi - array
+            string[] kurslar = new string[] { "a", "b", "c" }; 
+
+
+            for (int i=0; i<3; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+
+            }
+            Console.WriteLine("sayfa sonu -footer");
+        }
+
+    }
+
+
+}
